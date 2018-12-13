72cc9c1136587447074e205c51df3da23aca3739
devops <838626643@qq.com>
2018-12-13 14:30:10 +0800

    first commit

---
+++ local_initialization.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+git clone git@47.99.91.45:devops/webapp.git
+cd webapp
+a=`pwd`
+b="c.chinachelai.com"
+d="cl.chinachelai.com"
+read -p "please input your api_domain name: " c 
+read -p "please input your master domain name: " e
+function modify_directory()
+{
+      sed -i "s@/usr/nginx@$a@g" c/admin/gateway.lua
+      sed -i "s@/usr/nginx@$a@g" c/user/gateway.lua
+}
+function modify_domain_name()
+{
+      sed -i "s@$b@$c@g" che/app/index.html
+      sed -i "s@$b@$c@g" che/app/webapp/index.js
+      sed -i "s@$b@$c@g" che/script/services/appService.js
+      sed -i "s@$b@$c@g" conf/nginx.conf
+      sed -i "s@$d@$e@g" conf/nginx.conf
+}
+echo "--------初始化本地配置文件---------"
+modify_directory
+modify_domain_name
+echo "--------你可以修改代码了-----------"
+
