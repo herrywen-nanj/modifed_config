50478af200b4ddbfd3dcbd0ca06ed96f85f5131a
devops <838626643@qq.com>
2018-12-13 15:12:32 +0800

    update commit

--- local_initialization.sh
+++ local_initialization.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-git clone git@47.99.91.45:devops/webapp.git
+git clone git@47.99.91.45:devops/webapp.git > /dev/null  2>&1
 cd webapp
 a=`pwd`
 b="c.chinachelai.com"
 d="cl.chinachelai.com"
+echo "---------------请按照提示依次输入你的api域名，主域名，服务端口，如果不需要修改服务端口，可以直接回车-------------------------"
 read -p "please input your api_domain name: " c 
 read -p "please input your master_domain name: " e
 read -p "please input your service_port: " f
@@ -24,8 +25,8 @@ function modify_domain_name()
 		sed -i "s@80@$f@g" conf/nginx.conf
       fi
 }
-echo "--------初始化本地配置文件---------"
+echo "--------初始化本地配置文件中---------"
 modify_directory
 modify_domain_name
-echo "--------你可以修改代码了-----------"
+echo "--------你可以修改自己的代码了-----------"
 echo  你的登陆方式: http://$e:$f
--- python_exute.py
+++ python_exute.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python
+# - * - coding: utf-8 - * -
 import os
-os.system("./local_initialization.sh")
+def detectPort():
+ a = [80,90,91]
+ c = []
+#print 'a数组是' , a
+ for port in a:
+     command = "lsof -i:{} > /dev/null  2>&1".format(port)
+    #print '你的端口分别是' , command
+     if os.system(command) != 0:
+        c.append(port)
+ print '您可用的端口还有' + ','.join(str(i) for i in c)
+detectPort()
+
+def excute_localshell():
+ os.system("./local_initialization.sh")
+excute_localshell()
+print '该程序已经运行完毕'
