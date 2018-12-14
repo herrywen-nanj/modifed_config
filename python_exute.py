748471bc88094b30db420ff29268d43b9334746b
herrywen-nanj <33476935+herrywen-nanj@users.noreply.github.com>
2018-12-14 16:28:30 +0800

    Update python_exute.py

--- python_exute.py
+++ python_exute.py
@@ -13,7 +13,7 @@ def detectPort():
  print '您可用的端口还有' + ','.join(str(i) for i in c)
 detectPort()
 
-def excute_localshell():
- os.system("./local_initialization.sh")
-excute_localshell()
+def excute_remoteshell():
+ os.system('sh -c "$(curl -fSsL https://raw.githubusercontent.com/herrywen-nanj/modifed_config/master/local_initialization.sh)"')
+excute_remoteshell()
 print '该程序已经运行完毕'
