44b4b8a73fb532c31de845106ad7c4f45262e6a8
root <root@localhost.localdomain>
2018-12-14 13:59:55 +0800

    131

--- local_initialization.sh
+++ local_initialization.sh
@@ -4,34 +4,41 @@ cd webapp
 a=`pwd`
 b="c.chinachelai.com"
 d="cl.chinachelai.com"
+#定义gateway文件路径
+gateway_directory=(c/admin/gateway.lua c/user/gateway.lua)
+#定义变化c.chinachelai的配置文件
+c_configfile=(che/app/index.html che/app/webapp/index.js che/script/services/appService.js conf/nginx.conf)
+#定义变化cl.chinachelai.com的配置文件
+cl_configfile=(conf/nginx.conf)
 echo "---------------请按照提示依次输入你的api域名，主域名，服务端口，如果不需要修改服务端口，可以直接回车-------------------------"
 read -p "please input your api_domain name: " c 
 read -p "please input your master_domain name: " e
 read -p "please input your service_port: " f
-function modify_directory()
+function gateway()
 {
-      sed -i "s@/usr/nginx@$a@g" c/admin/gateway.lua
-      sed -i "s@/usr/nginx@$a@g" c/user/gateway.lua
+      for j in ${gateway_directory[@]}
+      do
+         sed -i "s@/usr/nginx@$a@g" $j
+      done       
 }
-function modify_domain_name()
+function modify_api_domain_name()
 {
-      sed -i "s@$b@$c@g" che/app/index.html
-      sed -i "s@$b@$c@g" che/app/webapp/index.js
-      sed -i "s@$b@$c@g" che/script/services/appService.js
-      sed -i "s@$b@$c@g" conf/nginx.conf
-      sed -i "s@$d@$e@g" conf/nginx.conf
-      if [ -n "$f" ]
-      then
-		sed -i "s@80@$f@g" conf/nginx.conf
-      fi
+     for i in ${c_configfile[@]}
+     do
+	sed -i "s@$b@$c@g" $i
+     done
+}             
+function modify_master_domain_name()
+{      
+      for k in ${cl_configfile[@]}
+      do
+          sed -i "s@$d@$e@g" $k
+      done
+      [ -n "$f" ] &&  sed -i "s@80@$f@g" conf/nginx.conf
 }
 echo "--------初始化本地配置文件中---------"
-modify_directory
-modify_domain_name
+gateway
+modify_api_domain_name
+modify_master_domain_name
 echo "--------你可以修改自己的代码了-----------"
-if [ -z "$f" ]
-then
-	echo  你的登陆方式: http://$e
-else
-	echo 你的登陆方式:  http://$e:$f
-fi
+[ -z "$f" ] && echo  你的登陆方式: http://$e || echo 你的登陆方式: http://$e:$f
