c4555be16e8d4868e063cab9bf4a583a5997c3ec
herrywen-nanj <33476935+herrywen-nanj@users.noreply.github.com>
2018-12-14 15:30:25 +0800

    Update local_initialization.sh

--- local_initialization.sh
+++ local_initialization.sh
@@ -40,6 +40,7 @@ function modify_master_domain_name()
       done
       [ -n "${service_port}" ] &&  sed -i "s@${local_port}@${service_port}@g" conf/nginx.conf
 }
+#数组中"S[@]"是给对象传递数组中的每个元素，而"S[*]"是将所有元素作一次传递给对象，像下面只能只能使用"S[*]"，而无法使用"S[@]"，如果不带双引号的话，二者是等价
 echo "--------初始化本地配置文件中---------"
 gateway "${gateway_directory[*]}"
 modify_api_domain_name "${c_configfile[*]}"
